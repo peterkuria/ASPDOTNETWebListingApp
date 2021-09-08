986cf8d050e49b5ba9cfafc260ffa0bd160e996d
peterkuria <peter.kuria.cloud@gmail.com>
2021-09-08 17:26:56 +0300

    Add project files.

---
+++ Controllers/GadgetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDOTNETWebListingApp.Controllers
+{
+    public class GadgetsController : Controller
+    {
+        // GET: Gadgets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDOTNETWebListingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //  optional can add return View(About);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            // note The view take the name of the View module - name of action
+            return View("ContactUs");
+        }
+    }
+}
